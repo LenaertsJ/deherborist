06fefb4cb2f397f5493780b66331947030a476ba
Julie Lenaerts <julielenaerts@gmail.com>
2021-05-19 16:22:24 +0200

    navbar changed to fixed. main page content added

--- components/content-box.jsx
+++ components/content-box.jsx
@@ -3,17 +3,17 @@
 function ContentBox() {
     return (
         <div className="content-box">
-            <h2 className="plant-name">brandnetel</h2>
-            <p>
-            Lorem ipsum dolor sit amet, consectetur adipis.Lorem ipsum dolor sit
-            amet, consectetur adipis Lorem ipsum dolor sit amet, consectetur
-            adipis Lorem ipsum dolor sit amet, consectetur adipis Lorem ipsum
-            dolor sit amet, consectetur adipis Lorem ipsum dolor sit amet,
-            consectetur adipis. Lorem ipsum dolor sit amet, consectetur adip Lorem
-            ipsum dolor sit amet, consectetur adipis Lorem ipsum dolor sit amet,
-            consectetur adipis Lorem ipsum dolor sit amet, consectetur adipis
-            Lorem ipsum dolor sit amet, consectetur adipis.
-            </p>
+        <h2 className="plant-name">brandnetel</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipis.Lorem ipsum dolor sit
+          amet, consectetur adipis Lorem ipsum dolor sit amet, consectetur
+          adipis Lorem ipsum dolor sit amet, consectetur adipis Lorem ipsum
+          dolor sit amet, consectetur adipis Lorem ipsum dolor sit amet,
+          consectetur adipis. Lorem ipsum dolor sit amet, consectetur adip Lorem
+          ipsum dolor sit amet, consectetur adipis Lorem ipsum dolor sit amet,
+          consectetur adipis Lorem ipsum dolor sit amet, consectetur adipis
+          Lorem ipsum dolor sit amet, consectetur adipis.
+        </p>
       </div>
     )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,48 @@
+import ContentBox from "../components/content-box";
+
 export default function Home() {
   return (
-    <main className="homepage">
-      <p className="quote">
-        "They say we have to nurture our garden, but of course it's our garden
-        that nurtures us..."
-      </p>
+    <main>
+      <div className="landingpage">
+        <p className="quote">
+          "They say we have to nurture our garden, but of course it's our garden
+          that nurtures us..."
+        </p>
+      </div>
+      <div id="site-overview">
+        <div class="overview-section">
+          <img src="/images/bundo-kim.jpeg" className="overview-img"></img>
+          <div className="overview-box">
+            <p className="overview-text">
+              Lorem ipsum dolor sit amet, consectetur adipis.Lorem ipsum dolor
+              sit amet, consectetur adipis Lorem ipsum dolor sit amet,
+              consectetur adipis Lorem ipsum dolor sit amet, consectetur adipis
+              Lorem ipsum dolor sit amet, consectetur adipis Lorem ipsum dolor
+              sit amet, consectetur adipis. Lorem ipsum dolor sit amet,
+              consectetur adip Lorem ipsum dolor sit amet, consectetur adipis
+              Lorem ipsum dolor sit amet, consectetur adipis Lorem ipsum dolor
+              sit amet, consectetur adipis Lorem ipsum dolor sit amet,
+              consectetur adipis.
+            </p>
+          </div>
+        </div>
+        <div class="overview-section">
+          <img src="/images/pressed_flower.jpg" className="overview-img"></img>
+          <div className="overview-box">
+            <p className="overview-text">
+              Lorem ipsum dolor sit amet, consectetur adipis.Lorem ipsum dolor
+              sit amet, consectetur adipis Lorem ipsum dolor sit amet,
+              consectetur adipis Lorem ipsum dolor sit amet, consectetur adipis
+              Lorem ipsum dolor sit amet, consectetur adipis Lorem ipsum dolor
+              sit amet, consectetur adipis. Lorem ipsum dolor sit amet,
+              consectetur adip Lorem ipsum dolor sit amet, consectetur adipis
+              Lorem ipsum dolor sit amet, consectetur adipis Lorem ipsum dolor
+              sit amet, consectetur adipis Lorem ipsum dolor sit amet,
+              consectetur adipis.
+            </p>
+          </div>
+        </div>
+      </div>
     </main>
   );
 }

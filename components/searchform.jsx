bb0756b5cb65611246a22623545e6e1e7852b9d5
Julie Lenaerts <julielenaerts@gmail.com>
2021-05-26 19:11:56 +0200

    detail page statically generated + selectfield default value set

--- components/searchform.jsx
+++ components/searchform.jsx
@@ -7,8 +7,8 @@ function Searchform({handleClickEvent, handleSubmit, handleChange, options}) {
         <p><span className="search-word">Search</span> based on <span><button className="btn" onClick={ handleClickEvent }>plant</button></span> or <span><button className="btn" onClick={ handleClickEvent }>medicinal quality</button></span>...</p>
         <form className="search-form" onSubmit={handleSubmit}>
             <div className="select-div">
-                <select name="plant" onChange={ handleChange } className="select-field">
-                    <option disabled selected className="select-item">-- select an option --</option>
+                <select name="plant" onChange={ handleChange } defaultValue={'DEFAULT'} className="select-field">
+                    <option disabled value="DEFAULT" className="select-item">-- select an option --</option>
                     {options.map((option) => <option value={option.name} key={option.id} className="select-item">{option.name}</option>)}
                 </select> 
             </div>
--- pages/plants/[...slug].js
+++ pages/plants/[...slug].js
@@ -1,14 +1,44 @@
 import Contentbox from "../../components/content-box";
+import axios from "axios";
 
-function PlantDetail() {
+function PlantDetail({ plant }) {
   return (
     <main className="container">
       <div className="inner-container">
-        <img className="plant-img" src="/images/nettle_drawing.jpeg"></img>
-        <Contentbox />
+        <img className="plant-img" src={plant.image}></img>
+        <Contentbox plant={plant} />
       </div>
     </main>
   );
+}
+
+export async function getStaticProps(context) {
+  const [id] = context.params.slug;
+  const plant = await axios
+    .get(`http://127.0.0.1:8000/api/plants/${id}`)
+    .then((response) => response.data)
+    .catch((error) => console.log(error));
+  console.log(plant);
+
+  return {
+    props: {
+      plant,
+    },
+  };
+}
+
+export async function getStaticPaths() {
+  const plants = await axios
+    .get(`http://127.0.0.1:8000/api/plants`)
+    .then((response) => response.data["hydra:member"])
+    .catch((error) => console.log(error));
+
+  return {
+    paths: plants.map((plant) => ({
+      params: { slug: [plant.id.toString(), plant.name] },
+    })),
+    fallback: "blocking",
+  };
 }
 
 export default PlantDetail;

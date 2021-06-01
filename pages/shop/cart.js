2b9f7be14f0bebb5eacfe082213b3eaf6e8e122b
Julie Lenaerts <julielenaerts@gmail.com>
2021-06-01 15:57:47 +0200

    styling cart page

--- pages/shop/cart.js
+++ pages/shop/cart.js
@@ -3,20 +3,24 @@ function Cart() {
   return (
     <main className="container">
       <div className="inner-cart">
-        <h3 className="cart-title">mandje van julie lenaerts</h3>
-        <div className="cart-products">
-          <div class="product-details">
-            <h4 className="product-title">Morning dew</h4>
-            <p className="product-category">infusies</p>
+        <h3 className="cart-title">
+          <span>m</span>andje van julie lenaerts
+        </h3>
+        <div class="cart-content flex">
+          <div className="cart-products flex">
+            <div class="product-details">
+              <h4 className="product-title">Product: Morning dew</h4>
+              <p className="product-category">Type: infusies</p>
+            </div>
+            <div className="product-price flex">
+              <p className="quantity">Aantal: 2</p>
+              <p className="price">Prijs: 4.5 €</p>
+            </div>
           </div>
-          <div className="product-price">
-            <p className="product-price-total">4.5 €</p>
-            <p className="product-quantity">2</p>
+          <div className="cart-totals flex">
+            <p className="quantity">Totaal aantal: 2</p>
+            <p className="price">Totale prijs: 9 €</p>
           </div>
-        </div>
-        <div className="cart-totals">
-          <p className="cart-totals-quantity">2</p>
-          <p className="cart-totals-price">9 €</p>
         </div>
       </div>
     </main>

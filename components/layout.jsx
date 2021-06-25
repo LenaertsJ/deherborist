03589b43d55faa7630b0ae1e30dda0eff28975b1
Julie Lenaerts <julielenaerts@gmail.com>
2021-06-25 14:21:30 +0200

    final push?

--- components/layout.jsx
+++ components/layout.jsx
@@ -1,13 +1,17 @@
 import Navbar from "./navbar"
 import Footer from './footer'
 import Head from "next/head";
+import { NextSeo } from 'next-seo';
 
 function Layout({ children }) {
     return (
         <>
+        <NextSeo 
+            title="De Herborist"
+            description="Charlotte Smolders verteld je als herborist meer over de geneeskracht en symboliek van planten. In de webshop kan je geschenkjes aankopen waarin de geneeskrachtige palnten verwerkt werden."
+            canonical="https://deherborist.vercel.app"
+        />
             <Head>
-                <title>De Herborist</title>
-                <meta name="description" content="Charlotte Smolders: herborist" />
                 <link rel="icon" href="/images/logo-herborist.svg" />
             </Head>
 
